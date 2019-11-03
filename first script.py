9291b4d841beaac1765fb6f89a065bf7a70b178b
Nalie <nalie@Nalies-MacBook-Pro.local>
2019-11-03 21:25:33 +0800

    my first change commit

---
+++ first script.py
@@ -0,0 +1,10 @@
+print ('hello world')
+age = input("how old are you")
+print age, "is too young"
+
+
+print ('nickname')
+print (3+4)
+print 'hello','world',
+print "this is also first line"
+print ('hello \n world')
